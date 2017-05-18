ba853bdb9c355438f9b613dd2285d6b51d09a633
Able Tsang <abletsang@gmail.com>
2017-05-18 10:01:48 -0700

    installed mongoose and connected with mongodb to save campgrounds

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
 
-var campgrounds = [];
-
+mongoose.connect("mongodb://localhost/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+// 	name: "Maple Hill",
+// 	image: "http://www.campsitephotos.com/photo/camp/722/feature_Patagonia_Lake-f2.jpg"
+// }, function(err, campground) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(campground);
+// 	}
+// });
 
 app.get("/", function(req, res) {
 	res.render("landingpage");
 });
 
 app.get("/campgrounds", function(req, res) {
-	res.render("campgrounds", {campgrounds: campgrounds});
+	Campground.find({}, function(err,campgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: campgrounds});
+		}
+	});
 });
 
 app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
 	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	res.redirect("campgrounds");
+	Campground.create(newCampground, function(err, allcampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("campgrounds");
+		}
+	});
+	
 });
 
 app.get("/campgrounds/new", function(req, res) {
