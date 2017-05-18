9049b60da85c0f34532ba6ed772546a04fe337c8
Able Tsang <abletsang@gmail.com>
2017-05-17 23:55:00 -0700

    added a new post route and form for adding new campgrounds, and printing these campgrounds out in a list afterwards

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+var campgrounds = [];
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
@@ -8,7 +12,19 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-	res.render("campgrounds");
+	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res) {
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	res.redirect("campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new");
 });
 
 app.listen(8080, function() {
